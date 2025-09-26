25dbfb3f2eaa262462f9a7b0286c61171e93bc89
Myslitel666 <tretyakov_02@bk.ru>
2025-09-26 13:16:50 +0500

    first commit

---
+++ src/routes/settings/ProjectBox.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Box } from "svelte-elegant";
+
+  export let href;
+</script>
+
+<a {href} class="box-container">
+  <Box width="100%" height="100%" variant="Hoverable" padding="20px"
+    ><slot /></Box
+  >
+</a>
+
+<style>
+  .box-container {
+    text-decoration: none;
+    width: 250px;
+    height: 250px;
+  }
+</style>
